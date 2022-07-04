ced4eda743d46abe68678e001252c9921254bd65
shagyeong <shagyeong21@gmail.com>
2022-07-04 23:49:19 +0900

    220703 update

---
+++ junction.h
@@ -0,0 +1,21 @@
+//정션(분기점) 클래스
+#include<iostream>
+using namespace std;
+
+class junction{
+    public:
+        //생성자
+        junction();
+        //멤버 변수
+        int a;
+        int b;
+        //멤버 메서드
+    private:
+        //멤버 변수
+        //멤버 메서드
+};
+
+junction::junction(){
+    a = 0;
+    b = 0;
+}
---
+++ track.h
@@ -0,0 +1,22 @@
+//트랙 리스트
+#include<iostream>
+using namespace std;
+
+class track{
+    public:
+        //생성자
+
+        //멤버 변수
+        //속성 변수
+        bool istop; //is track of platform : '역'의 트랙인가?
+        //수치적 변수
+        int length; // 트랙 길이
+        int slimit; //speed limit : 제한 속력
+        //연결 변수
+        //모든 간선은 양 끝의
+
+        //멤버 메서드
+    private:
+        //멤버 변수
+        //멤버 메서드
+};
