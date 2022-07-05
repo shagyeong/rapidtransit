3b4603646e34060599693e6c16cb044a786e8478
shagyeong <shagyeong21@gmail.com>
2022-07-05 11:56:30 +0900

    20220705

--- main.cpp
+++ main.cpp
@@ -2,3 +2,14 @@
 using namespace std;
 
 #include "junction.h"
+#include "station.h"
+
+int main(void){
+    //station s1;
+    station s2("k326", "일산");
+
+    //cout << s1.snumber << s1.sname << endl;
+    cout << s2.snumber << s2.sname << endl;
+
+    return 0;
+}
--- station.h
+++ station.h
@@ -1,1 +1,25 @@
 //역 클래스
+#include<iostream>
+using namespace std;
+
+class station{
+    public:
+        //생성자
+        station();
+        station(const char* snumv, const char* snamev);
+        //멤버 변수
+        char* snumber; //station number : 역 번호
+        char* sname; //station name : 역명
+        //멤버 메서드    
+    private:
+};
+
+station::station(){
+    //*snumber = "0000";
+    //*sname = "no name";
+}
+
+station::station(const char* snumv, const char* snamev){
+    *snumber = *snumv;
+    *sname = *snamev;
+}
