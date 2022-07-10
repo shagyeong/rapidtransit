7105dba7d0d3f8ea3e359e3b48fffd83bd450f8c
shagyeong <shagyeong21@gmail.com>
2022-07-10 20:50:27 +0900

    20220710

--- dynamic factors/main.cpp
+++ dynamic factors/main.cpp
@@ -4,8 +4,8 @@ using namespace std;
 #include "train.h"
 
 int main(void){
-    performance linekj_allstop_pf(3.5, 3.5, 70, 110, 8);
-    performance linekj_rapid_pf(3.5, 3.5, 110, 110, 8);
+    train linekj_allstop_pf(3.5, 3.5, 70, 110, 8);
+    train linekj_rapid_pf(3.5, 3.5, 110, 110, 8);
 
     cout << linekj_allstop_pf.acc << endl;
     return 0;
--- dynamic factors/train.h
+++ dynamic factors/train.h
@@ -1,24 +1,32 @@
 #include<iostream>
 using namespace std;
 
-//철도차량 성능
-class performance{
+//철도차량
+class train{
     public:
         //생성자
-        performance(double accv, double decv, int tspeedv, int mspeedv, int carsv);
+        train(double accv, double decv, int tspeedv, int mspeedv, int carsv);
+
         //멤버 변수
         double acc;     //가속력
         double dec;     //감속력
         int tspeed; //top speed : 영업 최고 속력
         int mspeed; //maximum speed : 설계 최고 속력
         int cars; //차량 수(몇 칸인가?)
+
+        //멤버 메서드
+        void standardtime(void);  //표준운전시격 산출 함수
     private:
 };
 
-performance::performance(double accv, double decv, int tspeedv, int mspeedv, int carsv){
+train::train(double accv, double decv, int tspeedv, int mspeedv, int carsv){
     acc = accv;
     dec = decv;
     tspeed = tspeedv;
     mspeed = mspeedv;
     cars = carsv;
+}
+
+void train::standardtime(void){
+    cout << "pass" << endl;
 }
