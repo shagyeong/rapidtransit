29b14dc0175975b02d4ea13ef6a58dcc568f05fb
shagyeong <shagyeong21@gmail.com>
2022-07-30 23:09:21 +0900

    dynamic factors update

--- dynamic factors/main.cpp
+++ dynamic factors/main.cpp
@@ -4,9 +4,5 @@ using namespace std;
 #include "train.h"
 
 int main(void){
-    train linekj_allstop_pf(3.5, 3.5, 70, 110, 8);
-    train linekj_rapid_pf(3.5, 3.5, 110, 110, 8);
-
-    cout << linekj_allstop_pf.acc << endl;
     return 0;
 }
