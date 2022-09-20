7369cbb6c853ae5e7383126be2f337ff57d34a29
shagyeong <shagyeong21@gmail.com>
2022-09-20 22:58:18 +0900

    demo update

---
+++ demo/standardtime.h
@@ -0,0 +1,14 @@
+class track;
+class junction;
+class station;
+
+//ss : 시작역, es : 정지역
+//두 역 사이에 하나 이상의 역이 존재하면 '미정차'를 가정하고 산출
+int* standardtime(station ss, station es){
+    if(){//상역->하역
+        //...getjd...
+    }
+    else{//하역->상역
+        //...getju...
+    }
+}
