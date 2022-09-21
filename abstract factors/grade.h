4eeda44912e51959f6fc9fa48a210701222db6d3
shagyeong <shagyeong21@gmail.com>
2022-09-21 16:18:24 +0900

    abstract factors update

--- abstract factors/grade.h
+++ abstract factors/grade.h
@@ -2,16 +2,14 @@
 #include<string>
 using namespace std;
 
-//실제로는 다른 디렉토리에서 include
-class station; 
+#include "./line.h"
+#include "../static factors/station.h"
 
 class grade{
     public:
-        grade(); //allstop
-        grade(station* station0);
-
+        grade();
     private:
+        line l;
         string name; //등급 명칭
-
-        station* stations; //첫 번재 정차역의 포인터
+        station stops[l.getstops()];
 };
