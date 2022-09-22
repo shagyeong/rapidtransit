eab105983df18867471a3677626b24c4ae5c0f14
shagyeong <shagyeong21@gmail.com>
2022-09-22 23:23:39 +0900

    demo update

--- demo/reachtime.h
+++ demo/reachtime.h
@@ -1,4 +1,8 @@
-void standardtime(void){
+/* 각 case별 시격 산출에 관한 수식과 자세한 설명은 별도의 폴더 또는 notion에 있음 */
+#include<stdio.h>
+#include<math.h>
+
+void reachtime(void){
     /* 제원 참조 생략 */
 
     /* 차량 제원 */
@@ -14,6 +18,7 @@ void standardtime(void){
 
     double v_acc; //가속 목표
     double v_dec; //감속 목표
+    double v_x; //peculiar case의 미지의 속력 x
 
     double d_acc; //가속 거리
     double d_dec; //감속 거리
@@ -62,9 +67,55 @@ void standardtime(void){
             t_dec = v_dec / dec; //감속
 
             t_track = t_acc + t_flat + t_dec;
+
+            if(v_lim <= v_lim_n){
+                /* 감속 없이 현재 속력(현재 트랙의 한계 속력)이 경계 속력이 됨*/
+                v_bor = v_lim;
+            }
+            else{
+                /* 감속해 다음 한계 속력에 도달 */
+                v_bor = v_lim_n;
+            }
         }
         else{
             /* peculiar case : flat 구간이 나타나기 전 감속해야 한다.*/
+            t_acc = ((-5 * v_bor) + sqrt(25 * pow(v_bor, 2) + 180 * acc * length)) / (5 * acc);
+            v_x = v_bor + t_acc * acc;
+
+            if(v_x <= v_lim_n){
+                /* peculiar 1 : 감속 없음 */
+                t_track = t_acc;
+                v_bor = v_x;
+            }
+            else{
+                /* peculiar 2 : 다음 한계 속력까지 감속해야 함 */
+                t_acc = (
+                    (-5) * (acc + dec) * v_bor +
+                    sqrt(
+                        25 * pow((acc + dec), 2) * pow(v_bor, 2) -
+                        5 * acc * (acc + dec) *(
+                            5 * pow(v_bor, 2) -
+                            5 * pow(v_lim_n, 2) -
+                            36 * dec * length
+                        )
+                    )
+                ) /
+                (5 * acc * (acc + dec));
+                t_dec = (
+                    (-5) * (acc + dec) * v_lim_n +
+                    sqrt(
+                        25 * pow((acc + dec), 2) * pow(v_lim_n, 2) -
+                        5 * dec * (acc + dec) * (
+                            5 * pow(v_lim_n, 2) -
+                            5 * pow(v_bor, 2) -
+                            36 * acc * length
+                        )
+                    )
+                ) /
+                (5 * dec * (acc + dec));
+                t_track = t_acc + t_dec;
+                v_bor = v_lim_n;
+            }
         }
     }
 }
