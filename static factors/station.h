f68be66544f0de4f8fb2f883cd43cb8b0a56b425
shagyeong <shagyeong21@gmail.com>
2022-09-22 23:48:56 +0900

    static factors update

--- static factors/station.h
+++ static factors/station.h
@@ -1,3 +1,6 @@
+/* 역 포인팅에 대해 무한 반복의 가능성 */
+/* setstation 적절히 수정 바람 */
+
 #pragma once
 #include<iostream>
 #include<string>
@@ -12,8 +15,9 @@ class station{
 
         void transit(junction* transit0); //정션으로 정차 위치 지정-가중치(트랙)의 거리를 차례로 출력
 
-        void setupstation(station upsatation); //단위역 이어붙이기-상행
-        void setdownstation(station downstation); //단위역 이어붙이기-하행
+        /* 단, 상, 하행역 설정 뿐 아니라 '단위역 이어붙이기'(각 역의 끝 정션을 포인팅)를 겸함 */
+        void setupstation(station us); //단위역 이어붙이기-상행
+        void setdownstation(station ds); //단위역 이어붙이기-하행
 
         void setname(string namev);
         void setnumber(string numberv);
@@ -28,6 +32,8 @@ class station{
         junction* getjulast(void);
         junction* getjdfirst(void);
         junction* getjdlast(void);
+        station* getupstation(void);
+        station* getdownstation(void);
     private:
         string name; //역명
         string number; //역번
@@ -37,6 +43,9 @@ class station{
         junction* julast; //상행 마지막 정션
         junction* jdfirst; //하행 첫 정션
         junction* jdlast; //하행 마지막 정션
+
+        station* upstation;
+        station* downstation;
 };
 
 station::station(){
@@ -46,6 +55,8 @@ station::station(){
     julast = NULL;
     jdfirst = NULL;
     jdlast = NULL;
+    upstation = NULL;
+    downstation = NULL;
 }
 station::station(string namev, string numberv){
     name = namev;
@@ -54,15 +65,27 @@ station::station(string namev, string numberv){
     julast = NULL;
     jdfirst = NULL;
     jdlast = NULL;
+    upstation = NULL;
+    downstation = NULL;
 }
 
-void station::setupstation(station upsatation){
-    julast->setjunction1(upsatation.getjufirst());
-    upsatation.jdlast->setjunction1(jdfirst);
+void station::setupstation(station us){
+    /* 역 포인팅 */
+    upstation = &us;
+    us.setdownstation(*this);
+    
+    /* 정션 포인팅 */
+    julast->setjunction1(us.getjufirst());
+    us.jdlast->setjunction1(jdfirst);
 }
-void station::setdownstation(station downstation){
-    jdlast->setjunction1(downstation.getjdfirst());
-    downstation.julast->setjunction1(jufirst);
+void station::setdownstation(station ds){
+    /* 역 포인팅 */
+    downstation = &ds;
+    ds.setupstation(*this);
+
+    /* 정션 포인팅 */
+    jdlast->setjunction1(ds.getjdfirst());
+    ds.julast->setjunction1(jufirst);
     
 }
 
@@ -102,4 +125,10 @@ junction* station::getjdfirst(void){
 }
 junction* station::getjdlast(void){
     return jdlast;
+}
+station* station::getupstation(void){
+    return upstation;
+}
+station* station::getdownstation(void){
+    return downstation;
 }
