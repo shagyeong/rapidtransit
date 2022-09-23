420629fa8e5a7f5df9074bcfed9eae9d7fb69694
shagyeong <shagyeong21@gmail.com>
2022-09-23 12:10:35 +0900

    dynamic factors update

--- dynamic factors/train.h
+++ dynamic factors/train.h
@@ -2,7 +2,6 @@
 #include<iostream>
 using namespace std;
 
-//철도차량
 class train{
     public:
         train();
@@ -29,3 +28,58 @@ class train{
         int mspeed; //maximum speed : 설계 최고 속력
         int cars; //차량 수(몇 칸인가?)
 };
+
+train::train(){
+    number = "no numbered train";
+    acc = 0;
+    dec = 0;
+    tspeed = 0;
+    mspeed = 0;
+    cars = 0;
+}
+train::train(string numberv, double accv, double decv, int tspeedv, int mspeedv, int carsv){
+    number = numberv;
+    acc = accv;
+    dec = decv;
+    tspeed = tspeedv;
+    mspeed = mspeedv;
+    cars = carsv;
+}
+
+void train::setnumber(string numberv){
+    number = numberv;
+}
+void train::setacc(double accv){
+    acc = accv;
+}
+void train::setdec(double decv){
+    dec = decv;
+}
+void train::settspeed(int tspeedv){
+    tspeed = tspeedv;
+}
+void train::setmspeed(int mspeedv){
+    mspeed = mspeedv;
+}
+void train::setcars(int carsv){
+    cars = carsv;
+}
+
+string train::getnumber(void){
+    return number;
+}
+double train::getacc(void){
+    return acc;
+}
+double train::getdec(void){
+    return dec;
+}
+int train::gettspeed(void){
+    return tspeed;
+}
+int train::getmspeed(void){
+    return mspeed;
+}
+int train::getcars(void){
+    return cars;
+}
